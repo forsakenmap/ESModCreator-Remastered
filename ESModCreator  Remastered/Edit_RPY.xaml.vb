58f4a108f2381be28d81041a445151f5089243d9
forsakenmap <r1qua@yandex.ru>
2020-06-17 00:34:53 +0300

    65
    Update

---
+++ ESModCreator  Remastered/Edit_RPY.xaml.vb
@@ -0,0 +1,115 @@
+﻿Imports System.Drawing
+Imports System.IO
+
+Public Class Edit_RPY
+    Private Sub Edit_Load(sender As Object, e As EventArgs) Handles MyBase.Loaded
+        preView.Source = BitmapToImage(My.Resources.No_in_this)
+    End Sub
+    Private Sub code_TextChanged(sender As Object, e As TextChangedEventArgs) Handles code.TextChanged
+        Try
+            'Замена Tab на 4 пробела
+            Dim posx As Integer = code.SelectionStart
+            If InStr(code.Text, "	", CompareMethod.Text) Then
+                code.Text = Replace(code.Text, "	", "    ")
+                posx += 4
+            End If
+            code.SelectionStart = posx
+            Dim currentStr As String = code.GetLineText(code.GetLineIndexFromCharacterIndex(code.SelectionStart))
+            num.Items.Clear()
+            For a As Integer = 1 To code.LineCount
+                num.Items.Add(a)
+            Next
+            num.SelectedIndex = code.GetLineIndexFromCharacterIndex(code.SelectionStart)
+            num.ScrollIntoView(num.SelectedItem)
+            code.SelectionStart = posx
+            Dim st As String
+            st = Replace(currentStr, "  ", "")
+            '  Dim usc As StreamReader = New StreamReader("UserCommands.txt")
+            ' While usc.Peek <> -1
+            'Dim an As String = usc.ReadLine
+            ' If InStr(an, st, CompareMethod.Text) Then
+            '      hints.Items.Add(an)
+            ' End If
+            ' End While
+            Try
+                hints.Items.Clear()
+                Dim tx As StreamReader = New StreamReader("hints/text.txt")
+                While tx.Peek <> -1
+                    Dim an As String = tx.ReadLine
+                    If InStr(an, st, CompareMethod.Text) Then
+                        hints.Items.Add(an)
+                    End If
+                End While
+                Dim com As StreamReader = New StreamReader("hints/commands.txt")
+                While com.Peek <> -1
+                    Dim an As String = com.ReadLine
+                    If InStr(an, st, CompareMethod.Text) Then
+                        hints.Items.Add(an)
+                    End If
+                End While
+                Dim j As StreamReader = New StreamReader("hints/bg.txt")
+                While j.Peek <> -1
+                    Dim an As String = j.ReadLine
+                    If InStr(an, st, CompareMethod.Text) Then
+                        hints.Items.Add(an)
+                    End If
+                End While
+                Dim cg As StreamReader = New StreamReader("hints/cg.txt")
+                While cg.Peek <> -1
+                    Dim an As String = cg.ReadLine
+                    If InStr(an, st, CompareMethod.Text) Then
+                        hints.Items.Add(an)
+                    End If
+                End While
+                Dim music As StreamReader = New StreamReader("hints/music.txt")
+                While music.Peek <> -1
+                    Dim an As String = music.ReadLine
+                    If InStr(an, st, CompareMethod.Text) Then
+                        hints.Items.Add(an)
+                    End If
+                End While
+
+                Dim sprites As StreamReader = New StreamReader("hints/sprites.txt")
+                While sprites.Peek <> -1
+                    Dim an As String = sprites.ReadLine
+                    If InStr(an, st, CompareMethod.Text) Then
+                        hints.Items.Add(an)
+                    End If
+                End While
+                Dim ambience As StreamReader = New StreamReader("hints/ambience.txt")
+                While ambience.Peek <> -1
+                    Dim an As String = ambience.ReadLine
+                    If InStr(an, st, CompareMethod.Text) Then
+                        hints.Items.Add(an)
+                    End If
+                End While
+                '    usc.Close()
+                j.Close()
+                ambience.Close()
+                com.Close()
+                cg.Close()
+                music.Close()
+                sprites.Close()
+            Catch ex As Exception
+            End Try
+        Catch ex As Exception
+            End Try
+    End Sub
+    Shared Function BitmapToImage(ByVal btm As Bitmap) As BitmapImage 'Преобразование Bitmap к ImageSource
+        Dim ms As New MemoryStream()
+        CType(btm, Bitmap).Save(ms, Imaging.ImageFormat.Bmp)
+        Dim image As New BitmapImage()
+        image.BeginInit()
+        ms.Seek(0, SeekOrigin.Begin)
+        image.StreamSource = ms
+        image.EndInit()
+        Return image
+    End Function
+    Private Sub num_SelectionChanged(sender As Object, e As RoutedEventArgs) Handles num.MouseUp
+        Try
+            code.SelectionStart = code.GetCharacterIndexFromLineIndex(num.SelectedIndex)
+            code.Focus()
+        Catch ex As Exception
+        End Try
+    End Sub
+End Class
---
+++ ESModCreator  Remastered/MainWindow.xaml.vb
@@ -0,0 +1,87 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Text
+Imports System.IO
+
+Class MainWindow
+    Private Sub Menu_Load(sender As Object, e As EventArgs) Handles MyBase.Loaded
+        Logos.Source = BitmapToImage(My.Resources.Logo)
+        Dim fontFamilies() As FontFamily
+        Dim installedFontCollection As New InstalledFontCollection()
+        fontFamilies = installedFontCollection.Families
+        Dim count As Integer = fontFamilies.Length
+        Dim j As Integer
+        Dim detect As Integer = 0
+        While j < count
+            If fontFamilies(j).Name = "DO NOT LEAN ON DOOR" Then
+                detect = 1
+            End If
+            j += 1
+        End While
+        If detect = 0 Then
+            If MsgBox("Для нормального отображения всех элементов программы рекомендую установить следующий шрифт (окно установки откроется автоматически при нажатии кнопки 'OK')", MsgBoxStyle.OkCancel) = 1 Then
+                Process.Start(Directory.GetCurrentDirectory + "/Font/DO_NOT_LEAN_ON_DOOR.otf")
+            End If
+        End If
+        If Not My.Computer.FileSystem.DirectoryExists("hints/") Then
+            My.Computer.FileSystem.CreateDirectory("hints/")
+        End If
+        If Not SearchFile("hints/ambience.txt") Then
+            CreateTXT(My.Resources.ambience, "hints/ambience.txt")
+        End If
+        If Not SearchFile("hints/bg.txt") Then
+            CreateTXT(My.Resources.bg, "hints/bg.txt")
+        End If
+        If Not SearchFile("hints/cg.txt") Then
+            CreateTXT(My.Resources.cg, "hints/cg.txt")
+        End If
+        If Not SearchFile("hints/commands.txt") Then
+            CreateTXT(My.Resources.commands, "hints/commands.txt")
+        End If
+        If Not SearchFile("hints/music.txt") Then
+            CreateTXT(My.Resources.music, "hints/music.txt")
+        End If
+        If Not SearchFile("hints/sprites.txt") Then
+            CreateTXT(My.Resources.sprites, "hints/sprites.txt")
+        End If
+        If Not SearchFile("hints/text.txt") Then
+            CreateTXT(My.Resources.text, "hints/text.txt")
+        End If
+    End Sub
+    Private Sub Project_button_Click(sender As Object, e As RoutedEventArgs) Handles Project_button.Click
+
+    End Sub
+    Private Sub Logo_Click(sender As Object, e As RoutedEventArgs) Handles Logos.MouseUp
+        Process.Start("https://vk.com/esmodcreator")
+    End Sub
+
+    Private Sub RPY_Edit_button_Click(sender As Object, e As RoutedEventArgs) Handles RPY_Edit_button.Click
+        Dim a As New Edit_RPY
+        a.Show()
+        Me.Close()
+    End Sub
+    Private Sub About_US_Click(sender As Object, e As RoutedEventArgs) Handles About_US.Click
+        Process.Start("https://vk.com/esmodcreator")
+    End Sub
+    Shared Function BitmapToImage(ByVal btm As Bitmap) As BitmapImage
+        Dim ms As New MemoryStream()
+        CType(btm, Bitmap).Save(ms, Imaging.ImageFormat.Bmp)
+        Dim image As New BitmapImage()
+        image.BeginInit()
+        ms.Seek(0, SeekOrigin.Begin)
+        image.StreamSource = ms
+        image.EndInit()
+        Return image
+    End Function
+    Shared Function SearchFile(ByVal name As String)
+        Return My.Computer.FileSystem.FileExists(name)
+    End Function
+    Shared Function CreateTXT(ByVal text, ByVal directory)
+        Try
+            My.Computer.FileSystem.WriteAllText(directory, text, False)
+            Return 1
+        Catch ex As Exception
+            Return 0
+        End Try
+    End Function
+End Class
+
